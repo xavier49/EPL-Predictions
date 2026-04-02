2f78f902472f5dc5d9dbc41ae7760e770583d1c3
Xavier Lee <xavierr@umich.edu>
2026-04-01 22:25:49 -0400

    packages

--- predictions.py
+++ predictions.py
@@ -7,3 +7,12 @@ Created on Wed Apr  1 22:13:52 2026
 """
 
 #scraping epl table from espn
+from datetime import datetime, timedelta
+import os
+import numpy as np
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+from scipy.stats import poisson
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
