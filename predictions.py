0dfa854259c4b48776f48d2449e0d06b531557e0
Xavier Lee <xavierr@umich.edu>
2026-04-02 23:27:05 -0400

    scraping fixture list

--- predictions.py
+++ predictions.py
@@ -6,34 +6,46 @@ Created on Wed Apr  1 22:13:52 2026
 @author: xavierlee
 """
 
-#scraping epl table from espn
+
 from datetime import datetime, timedelta
 import os
 import numpy as np
+import requests
 import pandas as pd
 import requests
+from io import StringIO
 from dotenv import load_dotenv
 from scipy.stats import poisson
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
+#scraping epl table from espn
 url = "https://www.espn.com/soccer/table"
 table = pd.read_html(url)
-# print(len(table))
-# print(table[0])
-# print(table[1])
 
 teams_raw = table[0]
 standings = table[1]
 
+#removing abbreviated team names and seperating position and team
 teams = pd.DataFrame()
-
 teams["position"] = (teams_raw.iloc[:,0].str.extract(r"^(\d+)").astype(int))
 teams["team"] = teams_raw.iloc[:,0].str.replace(r"^(\d+)", "", regex = True).str.replace(r"^[A-Z]{3}", "", regex = True).str.strip()
 
+standings.columns = ["gp", "w", "d", "l", "gf", "ga", "gd", "pts"]
 standings = standings.apply(lambda c: c.astype(int))
+epl_table = pd.concat([teams, standings], axis = 1)
 
 
-table = pd.concat([teams, standings], axis = 1)
-print(table)
+
+#scraping fixture list from football-data.org
+load_dotenv()
+api_key = os.getenv("FOOTBALL_API_KEY")
+headers = {"X-Auth-Token": api_key}
+url = "https://api.football-data.org/v4/competitions/PL/matches"
+response = requests.get(url, headers=headers)
+matches = response.json()
+fixtures = pd.json_normalize(matches["matches"])
+print(fixtures[["utcDate", "homeTeam.name", "awayTeam.name", "score.fullTime.home", "score.fullTime.away"]].head(10))
+
+
 
