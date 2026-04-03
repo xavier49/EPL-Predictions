c69024b7b257f6a0399576c63b681fbc01dad826
Xavier Lee <xavierr@umich.edu>
2026-04-02 23:06:53 -0400

    scraped table from espn

--- predictions.py
+++ predictions.py
@@ -17,4 +17,23 @@ from scipy.stats import poisson
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
-print("hello")
+url = "https://www.espn.com/soccer/table"
+table = pd.read_html(url)
+# print(len(table))
+# print(table[0])
+# print(table[1])
+
+teams_raw = table[0]
+standings = table[1]
+
+teams = pd.DataFrame()
+
+teams["position"] = (teams_raw.iloc[:,0].str.extract(r"^(\d+)").astype(int))
+teams["team"] = teams_raw.iloc[:,0].str.replace(r"^(\d+)", "", regex = True).str.replace(r"^[A-Z]{3}", "", regex = True).str.strip()
+
+standings = standings.apply(lambda c: c.astype(int))
+
+
+table = pd.concat([teams, standings], axis = 1)
+print(table)
+
